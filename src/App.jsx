d1c090d23e9d562aa5d5e8370eea33ad77808e91
kalpanachamling99 <kalpanachamling99@gmail.com>
2023-09-02 14:43:37 +0545

    add new json data

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,28 @@ import './stylesheets/master.scss';
 
 import BasicTable from './components/BasicTable';
 import PaginateTable from './components/PaginateTable';
-
+import ComplexTable from './components/complex/ComplexTable';
 function App() {
   const [showBasicTable,setShowBasicTable] = useState(false);
-  const [showpaginateTable,setShowPaginateTable] = useState(true);
+  const [showpaginateTable,setShowPaginateTable] = useState(false);
+  const [showComplexTable,setShowComplexTable] = useState(true)
 
-  const showTableHelper = (basic,paginate) =>{
+  const showTableHelper = (basic,paginate,complex) =>{
     setShowBasicTable(basic);
     setShowPaginateTable(paginate);
+    setShowComplexTable(complex);
   }
   const basicTableHandler = (e) =>{
     e.preventDefault();
-    showTableHelper(true,false);
+    showTableHelper(true,false,false);
   }
   const PaginateTableHandler = (e) => {
     e.preventDefault();
-    showTableHelper(false,true);
+    showTableHelper(false,true,false);
+  }
+  const complexTableHandler = (e)=>{
+    e.preventDefault();
+    showTableHelper(false,false,true);
   }
   return (
     <>
@@ -27,14 +33,16 @@ function App() {
         <ul>
           <li onClick={basicTableHandler}>Basic Table</li>
           <li onClick ={PaginateTableHandler}>Paginate Table</li>
-          <li>Sorting Table</li>
-          <li>Filter Table</li>
+          {/* <li>Sorting Table</li> */}
+          {/* <li>Filter Table</li> */}
+          <li onClick={complexTableHandler}>Complex Table</li>
         </ul>
       </div>
       <div className="content">
           <div className="container">
             {showBasicTable && <BasicTable />}
             {showpaginateTable && <PaginateTable />}
+            {showComplexTable && <ComplexTable />}
         </div>
       </div>
     </>
---
+++ src/components/complex/ComplexTable.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ComplexTable = () => {
+  return(
+    <div className="complex-table">
+      <h2>Complex Table</h2>
+    </div>
+  )
+}
+export default ComplexTable;
